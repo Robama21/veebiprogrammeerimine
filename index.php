38551ae2a9f208bc99dc0400fa296574b5f294c7
Robin Rannavete <robiran@users.noreply.com>
2018-09-11 14:22:23 +0300

    minu lisatud esimene fail

---
+++ index.php
@@ -0,0 +1,18 @@
+<DOCTYPE html>
+	<html>
+		<head>
+			<meta charset="utf-8">
+				<title>SKIDAADEL SKIDULLE
+				</title>
+		</head>
+			<body>
+				<h1>Guten tag!
+				</h1>
+				<p>nothing to see here. *flies away*
+				</p>
+				<img src="ykFr82X8gH-2.png">
+				<img src="tlu_terra_600x800_2.jpg" alt="TLÜ Terra õppehoone">
+				<a href="https://www.tlu.ee">Tallinna Ülikool</a>
+				<a href="../../~cauphel/">magic program man</a>
+			</body>
+	</html>
