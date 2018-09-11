892f42d18f23a7402a72d2672a049a63531c952d
Robin Rannavete <robiran@users.noreply.com>
2018-09-11 15:20:16 +0300

    big muutus

--- index.php
+++ index.php
@@ -1,15 +1,44 @@
+<?php
+	//echo "phphphphphphphphphp";
+	$firstName = "Robin";
+	$lastName = "Rannavete";
+	$dateToday = date("d/m/y");
+	$hourNow = date("d/m/y");
+	$partOfDay = " ";
+	if ($hourNow < 8) {
+	$partOfDay = "Varajane hommik";
+	}
+	if ($hourNow >= 8  and $hourNow < 16){
+	$partOfDay = "koolipäev";
+	}
+	if ($hourNow > 16) {
+	$partOfDay = "loodetavasti vaba aeg";
+	}
+?>
 <DOCTYPE html>
 	<html>
 		<head>
 			<meta charset="utf-8">
-				<title>SKIDAADEL SKIDULLE
-				</title>
+				<title>
+				<?php
+					echo $firstName;
+					echo " ";
+					echo $lastName;
+					
+				?>
+				õppetöö</title>
 		</head>
 			<body>
-				<h1>Guten tag!
+				<h1>
+				<?php
+					echo $firstName ." " .$lastName;
+				?>
 				</h1>
 				<p>nothing to see here. *flies away*
-				</p>
+					<?php
+					echo "Tänane kuupäev on " .$dateToday."</p>";
+					echo "<p>Lehe avamise hetkel oli " .$partOfDay."</p>";
+					?>
 				<img src="ykFr82X8gH-2.png">
 				<img src="tlu_terra_600x800_2.jpg" alt="TLÜ Terra õppehoone">
 				<a href="https://www.tlu.ee">Tallinna Ülikool</a>
